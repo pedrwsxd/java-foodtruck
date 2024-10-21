8f35a2a40870f022d6e1367d6757a0025dccab96
Pedro Henrick <pedrws15@gmail.com>
2024-10-21 12:29:52 -0300

    Correção

--- projetoifood/src/main/resources/db/migration/V1_1__create_table_usuarios.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE usuarios(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL,
-    role VARCHAR(50) NOT NULL
-);
-
-
