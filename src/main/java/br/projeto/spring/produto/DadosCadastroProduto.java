81617dc2ede438b24cb48bcbb4f5a26bfd322f76
Pedro_Henrick <Pedrwsxd@gmail.com>
2024-10-23 21:09:00 -0300

    Correções

---
+++ src/main/java/br/projeto/spring/produto/DadosCadastroProduto.java
@@ -0,0 +1,27 @@
+package br.projeto.spring.produto;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosCadastroProduto(
+		
+		@NotBlank
+		String nome,
+		
+		@NotBlank
+		String imagemUrl,
+		
+		@NotNull
+		BigDecimal preco,
+		
+		@NotBlank
+		String tipo,
+		
+		@NotBlank
+		String sabor
+		
+		) {
+
+}
