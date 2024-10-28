463c992d929835608c19f4e52aca72bc678582b2
Pedro Henrick <pedrws15@gmail.com>
2024-10-28 20:22:31 -0300

    Reestruturação

--- bin/main/db/migration/V1_3__create_table_pedidos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE pedidos (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    cliente BIGINT NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    data_pedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (cliente) REFERENCES usuarios(id)
-);
